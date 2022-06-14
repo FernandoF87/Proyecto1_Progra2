68d6b9b010da12bf72eae98686fec11e596ae7c6
jostin7799 <32961726+jostin7799@users.noreply.github.com>
2022-06-14 10:40:49 -0600

    Agregada clase main
    sdfdjkfsdkfslakjd

---
+++ ProyectoProgra1/src/dsjadkjfsdf.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @version
+ * @author Jostin Castro
+ */
+public class dsjadkjfsdf {
+    
+    public static void main(String[] args) {
+        
+    }
+
+}
